96e284df1d642ace015ff6eb922630ed054b2f54
NanoSmasher <NanoSmasher@users.noreply.github.com>
2014-01-21 12:57:36 -0500

    Create wk1_n2.c

---
+++ wk1_n2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+  int first,second;
+  printf ("Enter two integers: ");
+  scanf("%d %d", &first, &second);
+  printf("The user entered %d and %d", first, second);
+return 0;
+}

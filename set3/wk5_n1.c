c856c585f831be85704e1e56a920c2346b643e27
NanoSmasher <NanoSmasher@users.noreply.github.com>
2014-02-02 15:25:02 -0500

    Create wk5_n1.c

---
+++ set3/wk5_n1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    The following short questions have all appeared on old midterms and finals:
+    (a) Convert the hexadecimal number ABCD to a binary number.
+
+    (b) Consider the following four non-decimal numbers, where the subscripts indicate the base of each number:
+    A8_16 251_8 2222_4 10100111_2.
+    Which number is the smallest, and which is the largest?
+
+    (c) What is the binary representation of a, where a = 27_16 - 27_8 ?
+
+    (d) What is the octal (base 8) representation of the hexadecimal value B38?
+    */
+    printf("a) ABCD_16 = 43981 = 101010111100111_2 ");
+    printf("\n\n\n\n");
+    printf("A8_16      = 168");
+    printf("251_8      = 169");
+    printf("2222_4     = 170 < largest");
+    printf("10100111_2 = 167 < smallest");
+    printf("\n\n\n\n");
+    printf("a = 27_16 - 27_8 = 16 = 1000_2");
+    printf("\n\n\n\n");
+    printf("B38_16 = 5470_8");
+    printf("\n\n\n\n");
+    return 0;
+}

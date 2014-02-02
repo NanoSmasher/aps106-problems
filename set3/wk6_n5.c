458327c240b00cbf05143e9b7c2821092a76ba6f
NanoSmasher <NanoSmasher@users.noreply.github.com>
2014-02-02 15:28:44 -0500

    Create wk6_n5.c

---
+++ set3/wk6_n5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    Something a little more complicated. Write a program that asks a user to enter a word,
+    and then decides if the word is in alphabetical order. For example:
+
+    Enter a word: Beer
+    The word is in alphabetical order
+
+    or
+
+    Enter a word: SOS
+    The word is NOT in alphabetical order
+
+    Note that to compare letters, you’ll need to convert all letters to one case.
+    And if the user enters something other than a letter, the program should respond appropriately.
+    For example:
+
+    Enter a word: Gr8
+    That’s not a word!
+    */
+    char input, last='A', answer='?';
+
+    printf("Enter a word: ");
+    while ((input = getchar() )!= '\n') {
+            if (input>96 && input<123) {input -= 'a'-'A';}
+            if (input>64 && input<91){
+                if (input>=last) {last = input; answer='y';}
+                else {answer = 'n'; break;}
+            } else {answer = '?'; break;}
+    }
+
+    switch(answer){
+    case ('y'):
+        printf("\nThe word is in alphabetical order");
+        break;
+    case ('n'):
+        printf("\nThe word is NOT in alphabetical order");
+        break;
+    case ('?'):
+        printf("\nThat's not a word!");
+        break;
+    }
+
+
+    return 0;
+}

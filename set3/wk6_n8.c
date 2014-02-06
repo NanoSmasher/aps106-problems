5e05dc48a249f9e45155fe96b54c27001a46b873
NanoSmasher <NanoSmasher@users.noreply.github.com>
2014-02-06 17:57:37 -0500

    Update wk6_n8.c

--- set3/wk6_n8.c
+++ set3/wk6_n8.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define FILENAME "grades.txt"
+#define COPY "grades_copy.txt"
 
 int main(){
     /*
-    On BB I’ve posted a file grades.txt that contains a list of integer grades. Write a program that opens that file,
-    reads all of the grades, and writes the following information to the screeen: the total number of grades,
-    the number of grades above and below 50, and the average grade.
+    Another quick question. Write a program that creates a copy of grades.txt, by reading each grade from the original file,
+    and then immediately writing that value to a file of a different name.
     */
 
     FILE *fp = fopen(FILENAME, "r");
-    int num=0,sum=0,above=0,below=0,count=0;
+    FILE *cf = fopen(COPY, "w");
+    int first=1, read=0;
 
-    if (fp == NULL) {
+    if (fp == NULL || cf == NULL) {
         printf("ERROR: Could not open file.");
         return -1;
     }
 
-    while((fscanf(fp,"%d",&num)) != EOF){
-        if (num>50) above++;
-        if (num<50) below++;
-        sum += num;
-        count++;
+    while((fscanf(fp,"%d",&read)) != EOF){
+        if (first) {fprintf(cf, "%d", read); first=!first;}
+        else fprintf(cf, "\n%d", read);
     }
 
-    printf("Total number of grades: %d\n",count);
-    printf("Grades above 50: %d\n",above);
-    printf("Grades below 50: %d\n",below);
-    printf("Average grade (mean): %.2f%%\n", 1.*sum/count);
-
     fclose(fp);
+    fclose(cf);
+
     return 0;
 }

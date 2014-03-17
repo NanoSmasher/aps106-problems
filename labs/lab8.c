9c267e9a9ba332e0515bb0831514ae6ffa310629
NanoSmasher <NanoSmasher@users.noreply.github.com>
2014-03-17 14:45:51 -0400

    Create lab8.c

---
+++ labs/lab8.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+void getText(char[]);
+void printMorseCode(char a[], char b[][5]);
+
+void main(){
+    char m[26][5] = {   {".-"},     {"-..."},   {"–·–·"},   {"-.."},    {"."},      {"..-."},
+                        {"--."},    {"...."},   {".."},     {".---"},   {"-.-"},
+                        {".-.."},   {"--"},     {"-."},     {"---"},    {".--."},   {"--.-"},
+                        {".-."},    {"..."},    {"-"},      {"..-"},    {"...-"},
+                        {".--"},    {"-..-"},   {"-.--"},   {"--.."}                            };
+    char a[80];
+    getText(a);
+    printMorseCode(a,m);
+    return;
+}
+
+void getText(char a[]){
+    scanf("%[^!]", a);
+    int i; for (i = 0; i<strlen(a);i++) a[i] = toupper(a[i]);
+    return;
+}
+
+void printMorseCode(char a[], char b[][5]){
+    int i; for (i = 0; i<strlen(a);i++){
+        if (a[i] == ' ') printf("\n");
+        else printf("%s ", b[a[i]-'A']);
+    }
+    return;
+}
